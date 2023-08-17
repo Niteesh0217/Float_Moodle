3ec8c98b48744b81c58de931fd5f045d57a0faf3
Niteesh0217 <88593769+Niteesh0217@users.noreply.github.com>
2023-08-17 15:26:10 +0530

    Delete floatapp directory

--- floatapp/admin.py
+++
@@ -1,5 +0,0 @@
-from floatapp.models import UserProfile
-from django.contrib import admin
-
-# Register your models here.
-admin.site.register(UserProfile)
--- floatapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FloatappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'floatapp'
